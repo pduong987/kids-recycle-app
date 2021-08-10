a779f3d35f73d779266f4f1d4a98d557ec257319
Duong Pham <ptduong987@gmail.com>
2021-08-10 22:23:42 +1000

    Base working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
   def index
 
-    if current_user != nil
-      redirect_to :controller => 'listings', :action => 'index'
-    else
-      render "index"
-    end
+    render "index"
 
   end
 end
